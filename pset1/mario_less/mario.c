f0035443665ef1c5189b5a8e63cce0102da376a5
TimaJuma <61769420+TimaJuma@users.noreply.github.com>
2020-04-21 00:19:42 -0600

    Create mario.c

---
+++ pset1/mario_less/mario.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int h;
+    // prompt user to enter proper height value 
+    do
+    {
+        h = get_int("Enter the height\n");
+    }
+    while (h < 1 || h > 8);
+    int i, j; 
+    for (i = 0; i <= h - 1; i++)
+    {
+        for (j = 0; j < h - i - 1; j++) //spaces
+        {
+            printf(" ");
+        }
+        
+        for (j = 0; j <= i; j++) //hash before space
+        {
+            printf("#");
+        }  
+        printf("\n");  
+         
+    }
+}
